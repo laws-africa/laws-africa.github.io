c9bc3aa37bb5ef2351b8470bff12fb6e84923450
Musango <musangow@gmail.com>
2021-09-16 16:42:39 +0200

    app.js: for tocify logic take just innertext instead of innerhtml

--- js/app.js
+++ js/app.js
@@ -3,34 +3,33 @@ function tocify($content, $toc) {
 
   $toc.empty();
 
-  $content.find('h1, h2, h3, h4').each(function(i, heading) {
-    var li = document.createElement('li'),
-        a = document.createElement('a'),
-        top;
+  $content.find("h1, h2, h3, h4").each(function (i, heading) {
+    var li = document.createElement("li"),
+      a = document.createElement("a"),
+      top;
 
-    a.href = '#' + heading.id;
-    a.innerHTML = heading.innerHTML;
+    a.href = "#" + heading.id;
+    a.innerHTML = heading.innerText;
     li.appendChild(a);
 
     // top level
     if (!stack) {
       $toc.append(li);
       stack = [[heading.tagName, li.parentElement]];
-
     } else {
       // find sibling, stack is at h3 and we want h1
-      while (stack.length > 0 && stack[stack.length-1][0] > heading.tagName) stack.pop();
-      top = stack[stack.length-1];
+      while (stack.length > 0 && stack[stack.length - 1][0] > heading.tagName)
+        stack.pop();
+      top = stack[stack.length - 1];
 
       if (top[0] == heading.tagName) {
         // siblings
         top[1].appendChild(li);
-
       } else {
         // child
         // new sublist
-        ul = document.createElement('ul');
-        ul.className = 'list-unstyled ml-3';
+        ul = document.createElement("ul");
+        ul.className = "list-unstyled ml-3";
         ul.appendChild(li);
 
         top[1].lastElementChild.appendChild(ul);
@@ -40,8 +39,8 @@ function tocify($content, $toc) {
   });
 }
 
-$(function() {
-  $('[data-tocify]').each(function(i, content) {
-    tocify($(content), $(content.getAttribute('data-tocify')));
+$(function () {
+  $("[data-tocify]").each(function (i, content) {
+    tocify($(content), $(content.getAttribute("data-tocify")));
   });
 });
